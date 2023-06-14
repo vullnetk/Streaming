b5949f1781b45af9e6c89200ecd8e6333344f132
reartmydyti <rm51287@ubt-uni.net>
2023-06-14 21:20:06 +0200

    Cast Crew Add

--- client/src/views/CastCrews/AddCastCrew.vue
+++ client/src/views/CastCrews/AddCastCrew.vue
@@ -1,53 +1,68 @@
 <template>
-    <div>
-      <h2>Add Cast Crew</h2>
-      <input type="text" v-model="newCastCrew">
-      <select v-model="selectedRole">
-        <option v-for="role in roles" :key="role.id" :value="role.id">{{ role.role }}</option>
-      </select>
-      <button @click="addCastCrew">Add</button>
-    </div>
-  </template>
-  <script>
-  import { addCastCrew  } from '../../api/castCrew';
-  import { fetchCastCrewRoles  } from '../../api/castCrewRoles';
-  export default {
-    name: 'AddCastCrew',
-    data() {
-      return {
-        newCastCrew: '',
-        selectedRole: '',
-        roles: [],
-      };
-    },
-    mounted() {
-      this.fetchCastCrewRoles();
-    },
-    methods: {
-      fetchCastCrewRoles() {
-        fetchCastCrewRoles()
-          .then(roles => {
-            this.roles = roles;
-          })
-          .catch(error => {
-            console.error('Failed to fetch roles:', error);
-          });
-      },
-      addCastCrew() {
-        if(this.newCastCrew.trim() !== ''){
-        addCastCrew(this.newCastCrew, this.selectedRole)
-          .then(() => {
-            console.log('CastCrew added successfully');
-            this.newCastCrew = '';
-            this.selectedRole = '';
-          })
-          .catch(error => {
-            console.error('Failed to add CastCrew:', error);
-          });
-        }
-      }
-    }
-  };
-  </script>
+  <div class="form-container">
+      <h2>Upload Cast Crew </h2>
+      <div class="insert-form">
+          <b-form @submit="onSubmit">
+              <b-form-group
+                  label="Cast Crew Name"
+              >
+                  <b-form-input
+                      v-model="castCrew.fullName"
+                      placeholder="Cast Crew name"
+                      required
+                      
+                  ></b-form-input>
+                  
+              </b-form-group>
+          </b-form>
+      </div>
+  </div>
+</template>
 
+<script>
+import { addCastCrew } from '../../api/CastCrews'
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css'; 
+export default{
+  name: 'AddCastCrew',
+  data() {
+      return {
+          castCrew: {
+              fullName: ''
+          },
+      
+      }
+  },
   
+  methods: {
+      async onSubmit() {
+          
+          try{
+              await addCastCrew(this.castCrew.fullName)
+              this.resetForm()
+              toast("Upcoming product uploaded successfuly", {
+                  autoClose: 1000,
+              });
+          } catch {
+              toast("Upcoming product upload failed", {
+                  autoClose: 1000,
+              });
+          }
+      },
+
+      resetForm() {
+          this.castCrew.fullName = ""
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.form-container {
+  width: 600px;
+  padding: 3% 0;
+  margin: 0 auto;
+}
+
+</style>
